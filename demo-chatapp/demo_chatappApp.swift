5afbfce85c0bbdd71aeb3ced870efa7c5bed793d
Amal Zakirov <116391159+azaiv@users.noreply.github.com>
2024-07-09 13:30:49 +0500

    Initial Commit

---
+++ demo-chatapp/demo_chatappApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_chatappApp.swift
+//  demo-chatapp
+//
+//  Created by Amal Zakirov on 09.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct demo_chatappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
